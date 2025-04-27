0d0367f99f4a737f7cbaa3640c0950d3cac60140
baha zenbil <bzenbil19@gmail.com>
2025-04-27 03:04:28 +0300

    cors desteği açıldı

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { preloadAllConnections } from './tenant/tenant-db.service';
-import { preloadTenantDomains } from './tenant/preload-domains'; // ekle
-import { TenantDomainService } from './tenant/tenant-domain.service'; // ekle
+import { preloadTenantDomains } from './tenant/preload-domains';
+import { TenantDomainService } from './tenant/tenant-domain.service';
 
 async function bootstrap() {
-  await preloadAllConnections(); // organizasyon DB bağlantılarını hazırla
+  await preloadAllConnections();
 
   const app = await NestFactory.create(AppModule);
 
-  const domainService = app.get(TenantDomainService); // domain eşleşme servisini al
-  await preloadTenantDomains(domainService); // custom domainleri cache'e al
+  app.enableCors({
+    origin: '*', // Her yerden istek al
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // İzin verilen HTTP metodlar
+    credentials: true, // İstersen cookie/token desteği de ver
+  });
+
+  const domainService = app.get(TenantDomainService);
+  await preloadTenantDomains(domainService);
 
   await app.listen(3000);
 }
